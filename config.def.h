d95ed798639755cb5c57c9a39997743fa6adc9ca
Marko Kriska <marko.kriska@pm.me>
2022-12-23 15:10:16 +0100

    update

--- config.def.h
+++ config.def.h
@@ -4,11 +4,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:weight=medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
-/* Colour scheme */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
